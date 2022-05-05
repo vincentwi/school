29e1fd9b0c2ddcc626c40f8344a2307579b3380f
Vincent W <b00784879@essec.edu>
2022-05-05 23:54:47 +0800

    update

---
+++ spring22/T2/NLP/test.py
@@ -0,0 +1,17 @@
+
+print("hello world")
+
+import torch
+print(torch.__version__)
+print(torch.is_cuda_available())
+  
+# Defining main function
+def main():
+    print("hey there")
+    return 
+  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
